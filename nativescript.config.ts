6e696751cd2ebbc0c2164c5cbc97f557b83045e6
acortino <hello@acortino.me>
2023-02-19 17:19:10 +0100

    feat: add new font and use font for icon

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'me.acortino.billizy',
+  id: 'me.acortino.budgetrackr',
   appPath: 'src',
   appResourcesPath: 'App_Resources',
   android: {
